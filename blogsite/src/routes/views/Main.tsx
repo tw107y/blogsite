0484333ed09d792a0444d2dff6cf100136d19897
Gordon Ye <tw107y@gmail.com>
2024-02-04 12:31:52 +1100

    Set up header in the main page

--- blogsite/src/routes/views/Main.tsx
+++ blogsite/src/routes/views/Main.tsx
@@ -1,14 +1,26 @@
 // Home.js
 
-
-import React from 'react';
+import {useState, useEffect} from "react";
 import { useNavigate } from 'react-router-dom';
-import {Button} from "@mui/material"
-import "./../../App.css"
+import {Button} from "@mui/material";
+// import "./../../assets/Header.css";
+import heartLogo from "./../../assets/Heart.png";
+import ResponsiveAppBar from './ResponsiveAppbar';
 
 const Main = () => {
   const navigate = useNavigate();
+  const [td, setTd] = useState([]);
 
+  useEffect(()=>{
+    fetch('http://127.0.0.1:5000/api').then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+    }
+
+    ).then(data=>console.log(data))
+  },[]
+  )
   const handleButtonClick = () => {
     // Navigate to "/other-route" when the button is clicked
     navigate('/vite');
@@ -17,9 +29,10 @@ const Main = () => {
 
   return (
     <>
-      <h1>Main Page</h1>
-      <Button onClick={handleButtonClick}>Vite</Button>
-    </>
+      <ResponsiveAppBar></ResponsiveAppBar>
+       
+      <Button onClick={handleButtonClick}>{td}</Button>
+    </> 
   )
   
   
