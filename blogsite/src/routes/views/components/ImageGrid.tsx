f2d4af3b8e6734509d8aa7089b03d2165f89031c
Gordon Ye <tw107y@gmail.com>
2024-02-14 06:06:35 +1100

    Added starter component, delete later

---
+++ blogsite/src/routes/views/components/ImageGrid.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// const ImageGrid = ({ images}) => {
+//   return (
+//     <div>
+//       {images.map((image, index) => (
+//         <img key={index} src={image} alt={`Image ${index}`} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default ImageGrid;
---
+++ blogsite/src/routes/views/components/RandomButton.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// const RandomButton = ({ onClick }) => {
+//   return (
+//     <button onClick={onClick} style={{ margin: '1rem' }}>
+//       Show Random Images
+//     </button>
+//   );
+// };
+
+// export default RandomButton;
