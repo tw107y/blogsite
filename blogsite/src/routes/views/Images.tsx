1908f8c8a3a505c5c6d72cb8ef86b603af0ea4c6
Gordon Ye <tw107y@gmail.com>
2024-02-14 06:05:30 +1100

    added images tsx to load images into file

---
+++ blogsite/src/routes/views/Images.tsx
@@ -0,0 +1,29 @@
+import img0 from "./images/image0.png"
+import img1 from "./images/image1.png"
+import img2 from "./images/image2.png"
+import img3 from "./images/image3.png"
+import img4 from "./images/image4.png"
+import img5 from "./images/image5.png"
+import img6 from "./images/image6.png"
+import img7 from "./images/image7.png"
+import img8 from "./images/image8.png"
+import img9 from "./images/image9.png"
+import img10 from "./images/image10.png"
+import img11 from "./images/image11.png"
+import img12 from "./images/image12.png"
+
+export const imagesdb : {[key:string] : any} = {
+    "./images/image0.png" : img0,
+    "./images/image1.png" : img1,
+    "./images/image2.png" : img2,
+    "./images/image3.png" : img3,
+    "./images/image4.png" : img4,
+    "./images/image5.png" : img5,
+    "./images/image6.png" : img6,
+    "./images/image7.png" : img7,
+    "./images/image8.png" : img8,
+    "./images/image9.png" : img9,
+    "./images/image10.png" : img10,
+    "./images/image11.png" : img11,
+    "./images/image12.png" : img12,
+}
