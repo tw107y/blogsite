22522246b3f5007a3871865a657d1e4d6ed19122
Gordon Ye <tw107y@gmail.com>
2024-02-14 06:05:04 +1100

    created coding blog

---
+++ blogsite/src/routes/views/CodingBlog.tsx
@@ -0,0 +1,19 @@
+// Home.js
+
+import React from 'react';
+import "./../../App.css"
+
+const CodingBlog = () => {
+  return <>
+    <h1>Blog 1</h1>
+    <p>Learning css</p>
+    <p>setting up backend and connection</p>
+    <p>dk yet lol</p>
+    <p>c++ xcode stuff</p>
+    <p>flask - stackoverflow bettwe than gpt maybe</p>
+    <p>Learning LLM</p>
+    <p>CP</p>
+  </>;
+};
+
+export default CodingBlog;
